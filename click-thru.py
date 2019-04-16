11cf7cdfd13256bbf4a9514e96d0d3aab8d04b20
Chris Rahn <crahn2011@gmail.com>
2019-04-16 12:12:21 -0700

    Fit Random Forest via Grid Search

--- click-thru.py
+++ click-thru.py
@@ -7,7 +7,9 @@ Builds a random forest classifier for clicked (1) or no-click (0)
 Please see the README for info about the data tables
 '''
 
+import sys
 import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 
 # Load the data tables into separate pandas DataFrames
@@ -55,3 +57,33 @@ total_emails = emails_proc.shape[0]
 
 print(f'{num_opened / total_emails: 2.2f}% of the emails were opened')
 print(f'{num_clicked / total_emails: 2.2f}% of the emails were clicked')
+
+# Ask the user before fitting the Random Forest classifier
+if input('Continue fitting model? (y/n)') in ['n', 'N', 'no', 'No']:
+    print('Bye bye!')
+    sys.exit()
+
+# Take the processed email DataFrame and make a train/test split for validation
+X = emails_proc.drop(['user_opened', 'user_clicked'], axis=1, inplace=False)
+y = emails_proc['user_clicked']
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Define a Random Forest estimator
+forest = RandomForestClassifier(random_state=42)
+
+# Define a parameter grid search to find the best hyperparameters
+best_forest = GridSearchCV(forest,
+                           {'n_estimators': [5, 10, 50, 100, 150, 200]},
+                           scoring='f1_micro',
+                           cv=3,
+                           verbose=5,
+                           n_jobs=4)
+
+# Use the grid search to find the best combination of hyperparameters
+best_forest.fit(X_train, y_train)
+
+# Print the best cross-validation results and best params
+print('Done! I found that these parameters worked best:')
+print(f'{best_forest.best_params_}')
+print(f'These resulted in an F1 score of {best_forest.best_score_:.4f}')
+
